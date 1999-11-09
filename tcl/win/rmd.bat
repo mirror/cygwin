e892457c99201c3e40ff0db1c5dd85ae1d825f7b
jsm <jsm>
1999-11-09 01:28:42 +0000

    Initial revision

---
+++ tcl/win/rmd.bat
@@ -0,0 +1,25 @@
+@echo off
+rem RCS: @(#) $Id: rmd.bat,v 1.2 1999/01/26 03:53:41 jingham Exp $
+
+if not exist %1\tag.txt goto end
+
+echo Removing directory %1
+
+if "%OS%" == "Windows_NT" goto winnt
+
+cd %1
+if errorlevel 1 goto end
+del *.*
+cd ..
+rmdir %1
+if errorlevel 1 goto end
+goto success
+
+:winnt
+rmdir %1 /s /q
+if errorlevel 1 goto end
+
+:success
+echo deleted directory %1
+
+:end

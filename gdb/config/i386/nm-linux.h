eb7ae487d33453e86b614fd37f93d3c44df093f1
kettenis <kettenis>
2000-03-05 22:57:05 +0000

    2000-03-05  Mark Kettenis  <kettenis@gnu.org>
    	Allow GDB to run on Linux 2.0 again.
    	* config.in: Add HAVE_PTRACE_GETREGS.
    	* configure.in: Check if <sys/ptrace.h> defines PTRACE_GETREGS.
    	* configure: Regenerated.
    	* config/i386/nm-linux.h (CANNOT_FETCH_REGISTER,
    	CANNOT_STORE_REGISTER): New defines.
    	* i386-linux-nat.c (have_ptrace_getregs): New variable.
    	(PTRACE_XFER_TYPE, CANNOT_FETCH_REGISTER, fetch_register,
    	old_fetch_inferior_registers, CANNOT_STORE_REGISTER,
    	store_register, old_store_inferior_registers): Copied over from
    	`inptrace.c' as a temporary measure.
    	(fetch_regs, store_regs, fetch_fpregs, store_fpregs):
    	Conditionalize on HAVE_PTRACE_GETREGS.  Define stubs if
    	HAVE_PTRACE_GETREGS isn't defined.
    	(fetch_regs): Reset `have_ptrace_getregs' if ptrace call fails
    	with EIO.
    	(fetch_inferior_registers, store_inferior_registers): Fall back on
    	the method use in `infptrace.c' (by calling
    	old_fetch_inferior_registers and old_store_inferior_registers) if
    	`have_ptrace_getregs' isn't set.

--- gdb/config/i386/nm-linux.h
+++ gdb/config/i386/nm-linux.h
@@ -1,35 +1,34 @@
 /* Native support for GNU/Linux, for GDB, the GNU debugger.
-   Copyright (C) 1986, 1987, 1989, 1992, 1996, 1998
+   Copyright (C) 1986, 1987, 1989, 1992, 1996, 1998, 2000
    Free Software Foundation, Inc.
 
-This file is part of GDB.
+   This file is part of GDB.
 
-This program is free software; you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation; either version 2 of the License, or
-(at your option) any later version.
+   This program is free software; you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation; either version 2 of the License, or
+   (at your option) any later version.
 
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
 
-You should have received a copy of the GNU General Public License
-along with this program; if not, write to the Free Software
-Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.  */
+   You should have received a copy of the GNU General Public License
+   along with this program; if not, write to the Free Software
+   Foundation, Inc., 59 Temple Place - Suite 330,
+   Boston, MA 02111-1307, USA.  */
 
 #ifndef NM_LINUX_H
 #define NM_LINUX_H
 
 #include "i386/nm-i386v.h"
+#include "nm-linux.h"
 
 /* Return sizeof user struct to callers in less machine dependent routines */
 
 #define KERNEL_U_SIZE kernel_u_size()
 extern int kernel_u_size PARAMS ((void));
-
-/* Tell gdb that we can attach and detach other processes */
-#define ATTACH_DETACH
 
 #define U_REGS_OFFSET 0
 
@@ -63,8 +62,18 @@ extern int kernel_u_size PARAMS ((void));
 #include "solib.h"		/* Support for shared libraries. */
 #endif
 
+/* Override copies of {fetch,store}_inferior_registers in `infptrace.c'.  */
+#define FETCH_INFERIOR_REGISTERS
+
+/* Nevertheless, define CANNOT_{FETCH,STORE}_REGISTER, because we fall
+   back on the code `infptrace.c' (well a copy of that code in
+   `i386-linux-nat.c' for now) and we can access only the
+   general-purpose registers in that way.  */
+#define CANNOT_FETCH_REGISTER(regno) ((regno) >= NUM_GREGS)
+#define CANNOT_STORE_REGISTER(regno) CANNOT_FETCH_REGISTER (regno)
+
 extern CORE_ADDR
-i386_stopped_by_watchpoint PARAMS ((int));
+  i386_stopped_by_watchpoint PARAMS ((int));
 extern int
 i386_insert_watchpoint PARAMS ((int pid, CORE_ADDR addr, int len, int rw));
 extern int

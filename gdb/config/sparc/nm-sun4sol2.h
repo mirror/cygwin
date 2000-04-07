be6aed2ae8477f62a03dcb8ef8116e4a0b6fcbe3
nsd <nsd>
2000-04-07 01:14:08 +0000

    * sol-thread.c (GET_LWP, GET_THREAD, BUILD_LWP, BUILD_THREAD): 	Change to rely on PIDGET etc. 	* config/i386/nm-i386sol2.h (TARGET_HAS_WATCHPOINTS, 	TARGET_CAN_USE_HARDWARE_WATCHPOINT, HAVE_CONTINUABLE_WATCHPOINT, 	STOPPED_BY_WATCHPOINT, target_[insert/remove]_watchpoint): 	define.  Allow target to use procfs hardware watchpoints. 	* config/sparc/nm-sun4sol2.h: ditto. 	* config/i386/tm-i386sol2.h (PIDGET, TIDGET, MERGEPID): modify 	definitions to use 16 bits for the pid, 15 bits for the tid, and 	1 bit for the flag. 	* config/sparc/tm-sun4sol2.h: ditto. 	(SOFTWARE_SINGLE_STEP, SOFTWARE_SINGLE_STEP_P): undefine. 	* testsuite/gdb.threads/pthreads.exp (all_threads_running): Allow 	for more than 15 thread increments.

--- gdb/config/sparc/nm-sun4sol2.h
+++ gdb/config/sparc/nm-sun4sol2.h
@@ -1,21 +1,22 @@
 /* Native-dependent definitions for Sparc running SVR4.
    Copyright 1994 Free Software Foundation, Inc.
 
-This file is part of GDB.
+   This file is part of GDB.
 
-This program is free software; you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation; either version 2 of the License, or
-(at your option) any later version.
+   This program is free software; you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation; either version 2 of the License, or
+   (at your option) any later version.
 
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
 
-You should have received a copy of the GNU General Public License
-along with this program; if not, write to the Free Software
-Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.  */
+   You should have received a copy of the GNU General Public License
+   along with this program; if not, write to the Free Software
+   Foundation, Inc., 59 Temple Place - Suite 330,
+   Boston, MA 02111-1307, USA.  */
 
 /* Include the generic SVR4 definitions.  */
 
@@ -29,17 +30,31 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.  */
 
 #define PRSVADDR_BROKEN
 
-#ifdef HAVE_THREAD_DB_LIB
+#ifdef NEW_PROC_API	/* Solaris 6 and above can do HW watchpoints */
 
-#ifdef __STDC__
-struct objfile;
-#endif
+#define TARGET_HAS_HARDWARE_WATCHPOINTS
 
-#define target_new_objfile(OBJFILE) sol_thread_new_objfile (OBJFILE)
+/* The man page for proc4 on solaris 6 and 7 says that the system
+   can support "thousands" of hardware watchpoints, but gives no
+   method for finding out how many.  So just tell GDB 'yes'.  */
+#define TARGET_CAN_USE_HARDWARE_WATCHPOINT(TYPE, CNT, OT) 1
 
-void sol_thread_new_objfile PARAMS ((struct objfile *objfile));
+/* When a hardware watchpoint fires off the PC will be left at the
+   instruction following the one which caused the watchpoint.  
+   It will *NOT* be necessary for GDB to step over the watchpoint. */
+#define HAVE_CONTINUABLE_WATCHPOINT
 
-#define FIND_NEW_THREADS sol_find_new_threads
-void sol_find_new_threads PARAMS ((void));
+extern int procfs_stopped_by_watchpoint PARAMS ((int));
+#define STOPPED_BY_WATCHPOINT(W) \
+  procfs_stopped_by_watchpoint(inferior_pid)
 
-#endif
+/* Use these macros for watchpoint insertion/deletion.  */
+/* type can be 0: write watch, 1: read watch, 2: access watch (read/write) */
+
+extern int procfs_set_watchpoint PARAMS ((int, CORE_ADDR, int, int, int));
+#define target_insert_watchpoint(ADDR, LEN, TYPE) \
+        procfs_set_watchpoint (inferior_pid, ADDR, LEN, TYPE, 1)
+#define target_remove_watchpoint(ADDR, LEN, TYPE) \
+        procfs_set_watchpoint (inferior_pid, ADDR, 0, 0, 0)
+
+#endif /* NEW_PROC_API */
--- gdb/config/sparc/tm-sun4sol2.h
+++ gdb/config/sparc/tm-sun4sol2.h
@@ -2,34 +2,40 @@
    Copyright 1989, 1992, 1993, 1994, 1995, 1997, 1998
    Free Software Foundation, Inc.
 
-This file is part of GDB.
+   This file is part of GDB.
 
-This program is free software; you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation; either version 2 of the License, or
-(at your option) any later version.
+   This program is free software; you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation; either version 2 of the License, or
+   (at your option) any later version.
 
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
 
-You should have received a copy of the GNU General Public License
-along with this program; if not, write to the Free Software
-Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.  */
+   You should have received a copy of the GNU General Public License
+   along with this program; if not, write to the Free Software
+   Foundation, Inc., 59 Temple Place - Suite 330,
+   Boston, MA 02111-1307, USA.  */
 
 #include "sparc/tm-sparc.h"
 #include "tm-sysv4.h"
- 
+
+/* With Sol2 it is no longer necessary to enable software single-step,
+   since the /proc interface can take care of it for us in hardware.  */
+#undef SOFTWARE_SINGLE_STEP
+#undef SOFTWARE_SINGLE_STEP_P
+
 /* There are two different signal handler trampolines in Solaris2.  */
 #define IN_SIGTRAMP(pc, name) \
   ((name) \
    && (STREQ ("sigacthandler", name) || STREQ ("ucbsigvechandler", name)))
- 
+
 /* The signal handler gets a pointer to an ucontext as third argument
    if it is called from sigacthandler.  This is the offset to the saved
    PC within it.  sparc_frame_saved_pc knows how to deal with
-   ucbsigvechandler.  */  
+   ucbsigvechandler.  */
 #define SIGCONTEXT_PC_OFFSET 44
 
 #if 0	/* FIXME Setjmp/longjmp are not as well doc'd in SunOS 5.x yet */
@@ -59,7 +65,7 @@ extern int
 get_longjmp_target PARAMS ((CORE_ADDR *));
 
 #define GET_LONGJMP_TARGET(ADDR) get_longjmp_target(ADDR)
-#endif  /* 0 */
+#endif /* 0 */
 
 /* The SunPRO compiler puts out 0 instead of the address in N_SO symbols,
    and for SunPRO 3.0, N_FUN symbols too.  */
@@ -75,20 +81,6 @@ extern char *sunpro_static_transform_name PARAMS ((char *));
 #define HANDLE_SVR4_EXEC_EMULATORS
 
 /* Macros to extract process id and thread id from a composite pid/tid */
-#define PIDGET(pid) ((pid) & 0xffff)
-#define TIDGET(pid) (((pid) >> 16) & 0xffff)
-
-/* Macro to extract carry from given regset.  */
-#define PROCFS_GET_CARRY(regset) ((regset)[R_PSR] & PS_FLAG_CARRY)
-
-#ifdef HAVE_THREAD_DB_LIB
-
-extern char *solaris_pid_to_str PARAMS ((int pid));
-#define target_pid_to_str(PID) solaris_pid_to_str (PID)
-
-#else
-
-extern char *procfs_pid_to_str PARAMS ((int pid));
-#define target_pid_to_str(PID) procfs_pid_to_str (PID)
-
-#endif
+#define PIDGET(PID)		(((PID) & 0xffff))
+#define TIDGET(PID)		(((PID) & 0x7fffffff) >> 16)
+#define MERGEPID(PID, TID)	(((PID) & 0xffff) | ((TID) << 16))

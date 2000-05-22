6e0c4c3c8f3ca09f31941dbe6cd1c5e540aa69ac
cagney <cagney>
2000-05-22 05:43:39 +0000

    Eliminate make_cleanup_func from top.c

--- gdb/top.h
+++ gdb/top.h
@@ -1,22 +1,22 @@
 /* Top level stuff for GDB, the GNU debugger.
-   Copyright 1986, 1987, 1988, 1989, 1990, 1991, 1992, 1993, 1994
-   Free Software Foundation, Inc.
+   Copyright 1986-1994, 2000 Free Software Foundation, Inc.
 
-This file is part of GDB.
+   This file is part of GDB.
 
-This program is free software; you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation; either version 2 of the License, or
-(at your option) any later version.
+   This program is free software; you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation; either version 2 of the License, or
+   (at your option) any later version.
 
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
 
-You should have received a copy of the GNU General Public License
-along with this program; if not, write to the Free Software
-Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.  */
+   You should have received a copy of the GNU General Public License
+   along with this program; if not, write to the Free Software
+   Foundation, Inc., 59 Temple Place - Suite 330,
+   Boston, MA 02111-1307, USA.  */
 
 /* From top.c.  */
 extern char *line;
@@ -27,36 +27,26 @@ extern int inhibit_gdbinit;
 extern int epoch_interface;
 extern char gdbinit[];
 
-/* Generally one should use catch_errors rather than manipulating these
-   directly.  The exception is main().  */
-#if defined(HAVE_SIGSETJMP)
-#define SIGJMP_BUF		sigjmp_buf
-#define SIGSETJMP(buf)		sigsetjmp(buf, 1)
-#define SIGLONGJMP(buf,val)	siglongjmp(buf,val)
-#else
-#define SIGJMP_BUF		jmp_buf
-#define SIGSETJMP(buf)		setjmp(buf)
-#define SIGLONGJMP(buf,val)	longjmp(buf,val)
-#endif
-
-extern SIGJMP_BUF error_return;
-extern SIGJMP_BUF quit_return;
-
-extern void print_gdb_version PARAMS ((GDB_FILE *));
+extern void print_gdb_version (struct ui_file *);
 
 extern void source_command PARAMS ((char *, int));
 extern void cd_command PARAMS ((char *, int));
 extern void read_command_file PARAMS ((FILE *));
 extern void init_history PARAMS ((void));
 extern void command_loop PARAMS ((void));
+extern void simplified_command_loop PARAMS ((char *(*read_input_func) (char *),
+			       void (*execute_command_func) (char *, int)));
 extern int quit_confirm PARAMS ((void));
 extern void quit_force PARAMS ((char *, int));
 extern void quit_command PARAMS ((char *, int));
+extern void command_loop_marker (void *);
+extern int quit_cover PARAMS ((PTR));
+extern void execute_command PARAMS ((char *, int));
 
 /* This function returns a pointer to the string that is used
    by gdb for its command prompt. */
-extern char *get_prompt PARAMS((void));
- 
+extern char *get_prompt PARAMS ((void));
+
 /* This function copies the specified string into the string that
    is used by gdb for its command prompt. */
 extern void set_prompt PARAMS ((char *));
@@ -64,4 +54,15 @@ extern void set_prompt PARAMS ((char *));
 /* From random places.  */
 extern int mapped_symbol_files;
 extern int readnow_symbol_files;
-#define	ALL_CLEANUPS	((struct cleanup *)0)
+
+/* Perform _initialize initialization */
+extern void gdb_init PARAMS ((char *));
+
+/* For use by event-top.c */
+/* Variables from top.c. */
+extern int source_line_number;
+extern char *source_file_name;
+extern char *source_error;
+extern char *source_pre_error;
+extern int history_expansion_p;
+extern int server_command;

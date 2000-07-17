31ac7ad225a8cf102f6cc6cdf9006b9371fbb74f
jtc <jtc>
2000-07-17 18:11:56 +0000

    2000-07-17  matthew green  <mrg@redhat.com>
            * config/sparc/nm-nbsd.h (GDB_GREGSET_T, GDB_FPREGSET_T): Define.

--- gdb/config/sparc/nm-nbsd.h
+++ gdb/config/sparc/nm-nbsd.h
@@ -2,30 +2,29 @@
    Copyright (C) 1986, 1987, 1989, 1992, 1995, 1996
    Free Software Foundation, Inc.
 
-This file is part of GDB.
+   This file is part of GDB.
 
-This program is free software; you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation; either version 2 of the License, or
-(at your option) any later version.
+   This program is free software; you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation; either version 2 of the License, or
+   (at your option) any later version.
 
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
 
-You should have received a copy of the GNU General Public License
-along with this program; if not, write to the Free Software
-Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.  */
+   You should have received a copy of the GNU General Public License
+   along with this program; if not, write to the Free Software
+   Foundation, Inc., 59 Temple Place - Suite 330,
+   Boston, MA 02111-1307, USA.  */
 
 #ifndef NM_NBSD_H
 #define NM_NBSD_H
 
 /* Get generic NetBSD native definitions. */
 
-#include "nm-nbsd.h"
-
-#define FETCH_INFERIOR_REGISTERS
+#include "config/nm-nbsd.h"
 
 /* Before storing, we need to read all the registers.  */
 
@@ -53,5 +52,8 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.  */
 #define PTRACE_GETFPREGS PT_GETFPREGS
 #define PTRACE_SETREGS	 PT_SETREGS
 #define PTRACE_SETFPREGS PT_SETFPREGS
+
+#define GDB_GREGSET_T	struct reg
+#define GDB_FPREGSET_T	struct fpreg
 
 #endif /* NM_NBSD_H */

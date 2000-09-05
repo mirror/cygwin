3488d6695f3ab9a0062f2207f644fc7231fe992c
brolley <brolley>
2000-09-05 16:13:39 +0000

    2000-09-05  Dave Brolley  <brolley@redhat.com>
    	* sim.scm (sim-finish!): Honour the definition of FAST_P when calling
    	@cpu@_pbb_begin. Use 0 if FAST_P is not defined.

--- cgen/sim.scm
+++ cgen/sim.scm
@@ -1884,12 +1884,16 @@ struct scache {
       "--begin--" () () '(c-code VOID "\
   {
 #if WITH_SCACHE_PBB_@CPU@
-#ifdef DEFINE_SWITCH
+#if defined DEFINE_SWITCH || defined FAST_P
     /* In the switch case FAST_P is a constant, allowing several optimizations
        in any called inline functions.  */
     vpc = @cpu@_pbb_begin (current_cpu, FAST_P);
 #else
+#if 0 /* cgen engine can't handle dynamic fast/full switching yet.  */
     vpc = @cpu@_pbb_begin (current_cpu, STATE_RUN_FAST_P (CPU_STATE (current_cpu)));
+#else
+    vpc = @cpu@_pbb_begin (current_cpu, 0);
+#endif
 #endif
 #endif
   }

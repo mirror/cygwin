58f0e300881a3ac842ce44919b4539de760eccce
bje <bje>
2000-11-21 04:08:30 +0000

    2000-11-21  Ben Elliston  <bje@redhat.com>
    	* utils.scm (copyright-fsf): Add the year 2000.

--- cgen/utils.scm
+++ cgen/utils.scm
@@ -1153,7 +1153,7 @@
   (cons "\
 THIS FILE IS MACHINE GENERATED WITH CGEN.
 
-Copyright (C) 1996, 1997, 1998, 1999 Free Software Foundation, Inc.
+Copyright (C) 1996, 1997, 1998, 1999, 2000 Free Software Foundation, Inc.
 "
 	"\
 This program is free software; you can redistribute it and/or modify

229cb0fcf0de5abaee1d2bbc16d8fb06db7d0a8c
mrg <mrg>
2001-01-13 14:26:05 +0000

    [bsp/ChangeLog] 2001-01-13  matthew green  <mrg@redhat.com>
    	* configrun-sid.in: `load-accessor' becomes Harvard architecture
    	friendly.  Split into `load-accessor-insn' and `load-accessor-data'.
    
    [component/loader/ChangeLog]
    2001-01-13  matthew green  <mrg@redhat.com>
    
    	* compLoader.cxx (load_accessor): Delete member.
    	(load_accessor_insn, load_accessor_data): New bus* members.
    	(ctor): Initialise load_accessor_insn and load_accessor_data.
    	Add `load-accessor-insn' and `load-accessor-data' accessors
    	and delete `load-accessor'.
    	(load_function): Extend to 64 bit addresses, and have an instruction
    	space identifier.
    	(load_it): Check both load_accessor_insn and load_accessor_data are set,
    	and use the appropriate one.  Extend messages to meantion instruction or
    	data loads.
    	* elfload.h (fetchQuadLittle, fetchQuadBig, fetchQuad): New macros.
    	(PFLOAD): Make 64-bit & Harvard address space friendly
    	(sw-load-elf.txt): Document `load-accessor-insn' and `load-accessor-data'
    	accessors.
            * elfload.c (readElfFile): Support 64-bit ELF as well as 32-bit ELF.
    	* sw-load-elf.txt: Document `load-accessor-insn' and `load-accessor-data'
    	accessors.

--- sid/component/loader/elfload.h
+++ sid/component/loader/elfload.h
@@ -18,10 +18,13 @@
 #define BYTE(X,offset) (((X)[offset]) & 255)
 #define fetchShortLittle(addr) (BYTE((addr),1)*256+BYTE((addr),0))
 #define fetchWordLittle(addr) (fetchShortLittle (addr) + fetchShortLittle((addr) + 2) * 65536)
+#define fetchQuadLittle(addr) (fetchWordLittle ((addr)) + ((host_int_8)fetchWordLittle((addr) + 4) << 32))
 #define fetchShortBig(addr) (BYTE((addr),0)*256+BYTE((addr),1))
 #define fetchWordBig(addr) (fetchShortBig (addr) * 65536 + fetchShortBig((addr) + 2))
-#define fetchWord(ADDR,LITTLE) ((LITTLE) ? fetchWordLittle((ADDR)) : fetchWordBig((ADDR)))
+#define fetchQuadBig(addr) (((host_int_8)fetchWordBig ((addr)) << 32) + fetchWordBig((addr) + 4))
 #define fetchShort(ADDR,LITTLE) ((LITTLE) ? fetchShortLittle((ADDR)) : fetchShortBig((ADDR)))
+#define fetchWord(ADDR,LITTLE) ((LITTLE) ? fetchWordLittle((ADDR)) : fetchWordBig((ADDR)))
+#define fetchQuad(ADDR,LITTLE) ((LITTLE) ? fetchQuadLittle((ADDR)) : fetchQuadBig((ADDR)))
 
 /* 
    PFLOAD represents a function that will read file data. 
@@ -29,17 +32,26 @@
    raw file data. If DEST is NULL, then DEST2 should be used.
    OFFSET is the byte offset into the file to be read.
    AMOUNT is the number of bytes to read.
+   INSN_SPACE is true if this data should be loaded into instruction space.
 
    If OFFSET and AMOUNT are both negative, then the file should be
    closed (any remaining bytes are to be ignored). */
 
-typedef int (*PFLOAD)(char *dest, char *dest2, int offset, int amount);
+typedef unsigned long long host_int_8;	/* XXX */
+
+typedef int (*PFLOAD)(host_int_8 dest, char *dest2, host_int_8 offset, host_int_8 amount, int insn_space);
 /* Load an ELF executable into memory. FUNC is used to actually read the
    file. */
 extern int readElfFile(PFLOAD func, unsigned*, int*);
 
+#define EI_CLASS 4
+#define ELFCLASS64  2 /* 64 bit */
 #define EI_DATA 5
 #define ELFDATA2LSB 1 /* little endian */
 #define ELFDATA2MSB 2 /* big endian */
+
+/* Elf program header flags */
+#define PF_X	0x01	/* executable */
+#define PF_R	0x04	/* readable */
 
 #endif

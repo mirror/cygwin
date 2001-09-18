1840999e476fac657472e290784b1f475d8e713e
corinna <corinna>
2001-09-18 17:49:06 +0000

    * gdb.base/ending-run.c (main): Set stdout buffersize         to the same reasonable value for any target.         * gdb.base/ending-run.exp: Add a regular expression         to make testsuite happy on Sanyo Stormy16 target.

--- gdb/testsuite/gdb.base/ending-run.c
+++ gdb/testsuite/gdb.base/ending-run.c
@@ -2,26 +2,32 @@
  * <leaves-core-file-on-quit> bugs.
  */
 #include <stdio.h>
+#include <stdlib.h>
 
+#ifdef PROTOTYPES
+int callee (int x)
+#else
 int callee( x )
 int x;
+#endif
 {
     int y = x * x;
     return (y - 2);
 }
 
-main()
+int main()
 {
 
     int *p;
     int i;
 
     p = (int *) malloc( 4 );
-    
+    setvbuf (stdout, alloca (64), _IOLBF, 64);
     for (i = 1; i < 10; i++)
         {
             printf( "%d ", callee( i ));
             
         }
     printf( " Goodbye!\n" );
+    return 0;
 }

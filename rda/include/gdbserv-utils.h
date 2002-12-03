5c04f83f4edc3878306930f3c24d0601b0cb749a
kevinb <kevinb>
2002-12-03 03:22:07 +0000

    Revise gdbserv_{host,le,be}_bytes_{to,from}_reg() interfaces.  Also add support for mips64 targets.

--- rda/include/gdbserv-utils.h
+++ rda/include/gdbserv-utils.h
@@ -60,43 +60,65 @@ extern void gdbserv_ulonglong_to_reg (struct gdbserv *gdbserv,
 				      unsigned long long val,
 				      struct gdbserv_reg *reg);
 
-/* Convert between a REG and a buffer representing a numeric type.
-   Handle big endian and little endian cases explicitly.  */
+/* Convert between a REG and a buffer representing a (possibly signed)
+   numeric type.  Handle big endian and little endian cases explicitly.
+   When the source buffer is bigger than the destination buffer, the
+   least significant bytes (as appropriate for the endianess) are
+   transferred.  When the source buffer is smaller than the destination,
+   the most significant bytes of the destination are padded appropriately.
+   
+   Note that gdbserv_be_bytes_from_reg() and gdbserv_le_bytes_from_reg()
+   have a buffer length parameter, but not a register length
+   parameter.  This is because the register length was obtained from a
+   register packet sent by the debug client.  On the other hand,
+   gdbserv_be_bytes_to_reg() and gdbserv_le_bytes_to_reg() take both a
+   buffer length and the register length.  This is because we're
+   constructing a register (which will likely be sent to the client)
+   of a particular size.  */
 
 extern void gdbserv_be_bytes_to_reg (struct gdbserv *gdbserv,
 		                     const void *buf,
-		                     int len,
-		                     struct gdbserv_reg *reg);
+		                     int buflen,
+		                     struct gdbserv_reg *reg,
+				     int reglen,
+				     int sign_extend);
 
 extern void gdbserv_le_bytes_to_reg (struct gdbserv *gdbserv,
 		                     const void *buf,
 		                     int len,
-		                     struct gdbserv_reg *reg);
+		                     struct gdbserv_reg *reg,
+				     int reglen,
+				     int sign_extend);
 
 extern void gdbserv_be_bytes_from_reg (struct gdbserv *gdbserv,
 		                       void *buf,
-		                       int *lenp,
-                                       const struct gdbserv_reg *reg);
+		                       int buflen,
+                                       const struct gdbserv_reg *reg,
+				       int sign_extend);
 
 extern void gdbserv_le_bytes_from_reg (struct gdbserv *gdbserv,
 		                       void *buf,
-		                       int *lenp,
-                                       const struct gdbserv_reg *reg);
+		                       int buflen,
+                                       const struct gdbserv_reg *reg,
+				       int sign_extend);
 
-/* Convert between a REG and a buffer representing a native numeric
-   type.  These are just wrappers for the routines above, but are
-   useful nonetheless since they free the caller from having to
+/* Convert between a REG and a buffer representing a native unsigned
+   numeric type.  These are just wrappers for the routines above, but
+   are useful nonetheless since they free the caller from having to
    worry about byte order issues.  */
 
 extern void gdbserv_host_bytes_to_reg (struct gdbserv *gdbserv,
 		                       const void *buf,
-		                       int len,
-		                       struct gdbserv_reg *reg);
+		                       int buflen,
+		                       struct gdbserv_reg *reg,
+				       int reglen,
+				       int sign_extend);
 
 extern void gdbserv_host_bytes_from_reg (struct gdbserv *gdbserv,
 		                         void *buf,
-		                         int *lenp,
-                                         const struct gdbserv_reg *reg);
+		                         int buflen,
+                                         const struct gdbserv_reg *reg,
+					 int sign_extend);
 
 #ifdef __cplusplus
 }

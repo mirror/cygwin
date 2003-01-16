6a28f884c67f0a6d964f4cf12391c8d0d4dd9524
jjohnstn <jjohnstn>
2003-01-16 22:37:51 +0000

    2003-01-16  Joel Sherrill <joel@OARcorp.com>
            * libc/sys/rtems/include/limits.h, libc/sys/rtems/sys/param.h,
            libc/sys/rtems/sys/syslimits.h: Update to be in sync with what
            constants are defined in each file in the shared versions in
            libc/include.
            * libc/sys/rtems/crt0.c: Define extra symbols on SH and HP-PA to
            autoconf can link programs.
            * libc/include/machine/types.h: Explicitly specify signed on
            intXX_t types to ensure they are signed.

--- newlib/libc/sys/rtems/sys/param.h
+++ newlib/libc/sys/rtems/sys/param.h
@@ -36,11 +36,35 @@
  * SUCH DAMAGE.
  *
  *	@(#)param.h	8.3 (Berkeley) 4/4/95
- * $Id: param.h,v 1.1 2002/11/07 19:27:36 jjohnstn Exp $
+ * $Id: param.h,v 1.2 2003/01/16 22:37:54 jjohnstn Exp $
  */
 
 #ifndef _SYS_PARAM_H_
 #define _SYS_PARAM_H_
+
+/* from newlib's <sys/param.h> */
+
+#include <sys/config.h>
+
+#ifndef BIG_ENDIAN
+#define BIG_ENDIAN 4321
+#endif
+#ifndef LITTLE_ENDIAN
+#define LITTLE_ENDIAN 1234
+#endif
+
+# define HZ (60)
+# define PATHSIZE (1024)
+
+#ifndef BYTE_ORDER
+#ifdef __IEEE_LITTLE_ENDIAN
+#define BYTE_ORDER LITTLE_ENDIAN
+#else
+#define BYTE_ORDER BIG_ENDIAN
+#endif
+#endif
+
+/* end of from newlib's <sys/param.h> */
 
 #include <unistd.h>
 

9d6082caa195cf93a27466aa5318b4ef19c4091f
cgf <cgf>
2004-02-10 16:15:52 +0000

    * strace.cc: Update copyrights. * cygcheck.cc: Update copyrights.

--- winsup/utils/cygcheck.cc
+++ winsup/utils/cygcheck.cc
@@ -1,6 +1,6 @@
 /* cygcheck.cc
 
-   Copyright 1998, 1999, 2000, 2001, 2002, 2003 Red Hat, Inc.
+   Copyright 1998, 1999, 2000, 2001, 2002, 2003, 2004 Red Hat, Inc.
 
    This file is part of Cygwin.
 
@@ -40,7 +40,7 @@ void dump_setup (int, char **, bool);
 void package_find (int, char **);
 void package_list (int, char **);
 
-static const char version[] = "$Revision: 1.41 $";
+static const char version[] = "$Revision: 1.42 $";
 
 static const char *known_env_vars[] = {
   "c_include_path",
@@ -1380,7 +1380,7 @@ print_version ()
   printf ("\
 cygcheck version %.*s\n\
 System Checker for Cygwin\n\
-Copyright 1998, 1999, 2000, 2001, 2002, 2003 Red Hat, Inc.\n\
+Copyright 1998, 1999, 2000, 2001, 2002, 2003, 2004 Red Hat, Inc.\n\
 Compiled on %s\n\
 ", len, v, __DATE__);
 }

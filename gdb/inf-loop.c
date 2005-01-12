6bca9fca45ce0a40de26f892a41558118d7974a0
cagney <cagney>
2005-01-12 18:31:24 +0000

    2005-01-12  Andrew Cagney  <cagney@gnu.org>
    	* exceptions.h (enum return_reason, RETURN_MASK)
    	(RETURN_MASK_QUIT, RETURN_MASK_ERROR, RETURN_MASK_ALL)
    	(return_mask, throw_exception, catch_exceptions_ftype)
    	(catch_exceptions_with_msg, catch_errors_ftype, catch_errors)
    	(catch_command_errors_ftype, catch_command_errors): Move to
    	exceptions.h.
    	* exceptions.c, exceptions.h: New files.
    	* top.c: Do not include <setjmp.h>.
    	(SIGJMP_BUF, SIGSETJMP, SIGLONGJMP, catch_return)
    	(throw_exception, catcher, catch_exceptions)
    	(catch_exceptions_with_msg, struct catch_errors_args)
    	(do_catch_errors, catch_errors, struct captured_command_args)
    	(do_captured_command, catch_command_errors): Move to exceptions.c.
    	* wrapper.c, wince.c, win32-nat.c, utils.c: Include "exceptions.h".
    	* tui/tui-interp.c, top.c, thread.c, symmisc.c: Ditto.
    	* symfile-mem.c, stack.c, solib.c, rs6000-nat.c: Ditto.
    	* remote-sds.c, remote-mips.c, remote-fileio.c: Ditto.
    	* remote-e7000.c, objc-lang.c, ocd.c: Ditto.
    	* remote.c, nto-procfs.c, monitor.c, mi/mi-main.c: Ditto.
    	* main.c, m32r-rom.c, infrun.c, inf-loop.c: Ditto.
    	* hppa-hpux-tdep.c, frame.c, event-top.c, event-loop.c: Ditto.
    	* corelow.c, corefile.c, cli/cli-interp.c, breakpoint.c: Ditto.
    	* ada-valprint.c, ada-lang.c: Ditto.
    	* Makefile.in (HFILES_NO_SRCDIR, COMMON_OBS): Add exceptions.h and
    	exceptions.o.  Update all dependencies.

--- gdb/inf-loop.c
+++ gdb/inf-loop.c
@@ -25,9 +25,17 @@
 #include "event-loop.h"
 #include "event-top.h"
 #include "inf-loop.h"
+#include "remote.h"
+#include "exceptions.h"
 
 static int fetch_inferior_event_wrapper (gdb_client_data client_data);
 static void complete_execution (void);
+
+void
+inferior_event_handler_wrapper (gdb_client_data client_data)
+{
+  inferior_event_handler (INF_QUIT_REQ, client_data);
+}
 
 /* General function to handle events in the inferior. So far it just
    takes care of detecting errors reported by select() or poll(),
@@ -71,7 +79,19 @@ inferior_event_handler (enum inferior_event_type event_type,
       complete_execution ();
       break;
 
-    case INF_QUIT_REQ:
+    case INF_EXEC_CONTINUE:
+      /* Is there anything left to do for the command issued to
+         complete? */
+      do_all_intermediate_continuations ();
+      break;
+
+    case INF_QUIT_REQ: 
+      /* FIXME: ezannoni 1999-10-04. This call should really be a
+	 target vector entry, so that it can be used for any kind of
+	 targets. */
+      async_remote_interrupt_twice (NULL);
+      break;
+
     case INF_TIMER:
     default:
       printf_unfiltered ("Event type not recognized.\n");

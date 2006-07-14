1657b043276d7ae524d97b5125cc990dfed4ccc4
brolley <brolley>
2006-07-14 19:42:53 +0000

    2006-07-14  Dave Brolley  <brolley@redhat.com>
            * stdio.cxx (stdioConsole): Initialize eof_p. Add "eof" pin.
            (read): Remove 'value'. Check eof_p. Drive eof_pin and set
            eof_p as needed.
            * components.h (eof_pin): New member of stdioConsole.
            (eof_p): Likewise.
            * sid-io-stdio.xml: Document eof pin.
            * sid-io-stdio.txt: Regenerated.

--- sid/component/consoles/stdio.cxx
+++ sid/component/consoles/stdio.cxx
@@ -1,7 +1,7 @@
 // stdio.cxx - A simple console that uses standard I/O for
 // enunciation.  -*- C++ -*-
 
-// Copyright (C) 1999, 2000, 2003 Red Hat.
+// Copyright (C) 1999, 2000, 2003, 2006 Red Hat.
 // This file is part of SID and is licensed under the GPL.
 // See the file COPYING.SID for conditions for redistribution.
 
@@ -13,11 +13,13 @@ namespace console_stuff
 
 stdioConsole::stdioConsole()
   :activity_pin(this, & stdioConsole::read),
-   stdout_pin(this, & stdioConsole::write)
+   stdout_pin(this, & stdioConsole::write),
+   eof_p (false)
 {
   add_pin("poll", &activity_pin);
   add_pin("stdin", &stdin_pin);
   add_pin("stdout", &stdout_pin);
+  add_pin("eof", &eof_pin);
   add_attribute_virtual ("state-snapshot", this,
 			 & stdioConsole::save_state,
 			 & stdioConsole::restore_state);
@@ -32,20 +34,28 @@ stdioConsole::write(host_int_4 value)
 void
 stdioConsole::read(host_int_4)
 {
+  // Check for EOF
+  if (eof_p)
+    return;
+
   unsigned char buf[1000];
   int len;
-  host_int_4 value;
-
   // Switch to non-blocking input.
   long flags = fcntl(0, F_GETFL);
   fcntl(0, F_SETFL, flags | O_NONBLOCK);
 
   if ((len = ::read(0, buf, 1000)) > 0)
     {
+      eof_pin.drive (0);
       for (int i = 0; i < len; ++i)
 	{
 	  stdin_pin.drive(buf[i]);
 	}
+    }
+  else if (len == 0)
+    {
+      eof_p = true;
+      eof_pin.drive (1);
     }
 
   // Restore flags.

9498188fa7419ab40bc98593e5165ebaf46e8223
drow <drow>
2009-01-20 15:33:13 +0000

    PR gdb/9346 	* infcmd.c (signal_command): Do not specify a resume PC.
    	testsuite/
    	PR gdb/9346
    	* gdb.base/interrupt.c (sigint_handler): New.
    	(main): Install a SIGINT handler if SIGNALS is defined.  Exit
    	on error.
    	* gdb.base/interrupt.exp: Define SIGNALS unless gdb,nosignals.
    	Test "signal SIGINT".

--- gdb/testsuite/gdb.base/interrupt.c
+++ gdb/testsuite/gdb.base/interrupt.c
@@ -1,5 +1,17 @@
 #include <errno.h>
 #include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+
+#ifdef SIGNALS
+#include <signal.h>
+
+static void
+sigint_handler (int signo)
+{
+}
+#endif
+
 int
 main ()
 {
@@ -8,6 +20,9 @@ main ()
 #ifdef usestubs
   set_debug_traps();
   breakpoint();
+#endif
+#ifdef SIGNALS
+  signal (SIGINT, sigint_handler);
 #endif
   printf ("talk to me baby\n");
   while (1)
@@ -18,7 +33,10 @@ main ()
 #ifdef EINTR
 	  if (errno != EINTR)
 #endif
-	    perror ("");
+	    {
+	      perror ("");
+	      return 1;
+	    }
 	}
       else if (nbytes == 0)
 	{
@@ -28,6 +46,7 @@ main ()
       else
 	write (1, &x, 1);
     }
+  return 0;
 }
 
 int

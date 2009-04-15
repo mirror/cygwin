9c6283739a6d0b05ab281ea9c7ac139557945076
corinna <corinna>
2009-04-15 08:20:47 +0000

    * libc/bsdlib.cc: Align copyright with upstream. 	* libc/fnmatch.c: Ditto. 	* libc/fts.c: Ditto. 	* libc/inet_addr.c: Ditto. 	* libc/inet_network.c: Ditto. 	* libc/rcmd.cc: Ditto. 	* libc/rexec.cc: Ditto.

--- winsup/cygwin/libc/inet_addr.c
+++ winsup/cygwin/libc/inet_addr.c
@@ -10,10 +10,6 @@
  * 2. Redistributions in binary form must reproduce the above copyright
  *    notice, this list of conditions and the following disclaimer in the
  *    documentation and/or other materials provided with the distribution.
- * 3. All advertising materials mentioning features or use of this software
- *    must display the following acknowledgement:
- * 	This product includes software developed by the University of
- * 	California, Berkeley and its contributors.
  * 4. Neither the name of the University nor the names of its contributors
  *    may be used to endorse or promote products derived from this software
  *    without specific prior written permission.
@@ -70,7 +66,7 @@
 
 #if defined(LIBC_SCCS) && !defined(lint)
 static const char sccsid[] = "@(#)inet_addr.c	8.1 (Berkeley) 6/17/93";
-static const char rcsid[] = "$Id: inet_addr.c,v 1.2 2006/07/10 08:15:54 corinna Exp $";
+static const char rcsid[] = "$Id: inet_addr.c,v 1.3 2009/04/15 08:20:47 corinna Exp $";
 #endif /* LIBC_SCCS and not lint */
 #include <sys/cdefs.h>
 #ifndef __CYGWIN__

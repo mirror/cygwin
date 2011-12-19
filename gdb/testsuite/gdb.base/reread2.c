7a7cdfee50fcb8e485eb1a64fa0c8b06f95636a7
jkratoch <jkratoch>
2011-12-19 22:28:22 +0000

    gdb/ 	* symfile.c (reread_symbols): Move free_objfile_separate_debug, 	preserve_values, sym_finish and clear_objfile_data calls before BFD 	close.  Move free_objfile_separate_debug as the very first call.  New 	comment on the ordering.
    gdb/testsuite/
    	* gdb.base/reread.exp: If srcfile2 fails to build retry it with
    	-DNO_SECTIONS.
    	* gdb.base/reread2.c <!NO_SECTIONS>: New sections block.

---
+++ gdb/testsuite/gdb.base/reread2.c
@@ -0,0 +1,32 @@
+/* pr 13484 */
+
+#include <stdio.h>
+
+int x;
+
+void foo()
+{
+  x++;
+  printf("This is foo\n");
+}
+
+int main()
+{
+#ifdef usestubs
+  set_debug_traps ();
+  breakpoint ();
+#endif
+  foo();
+  return 0;
+}
+
+/* Ensure the new file will have more sections.  It may exploit code not
+   updating its SECTION_COUNT on reread_symbols.  */
+
+#ifndef NO_SECTIONS
+# define VAR0(n) __attribute__ ((section ("sect" #n))) int var##n;
+# define VAR1(n) VAR0 (n ## 0) VAR0(n ## 1) VAR0(n ## 2) VAR0(n ## 3)
+# define VAR2(n) VAR1 (n ## 0) VAR1(n ## 1) VAR1(n ## 2) VAR1(n ## 3)
+# define VAR3(n) VAR2 (n ## 0) VAR2(n ## 1) VAR2(n ## 2) VAR2(n ## 3)
+VAR3 (0)
+#endif

d14d29e5656563bb6338500c0de42602affd22e5
corinna <corinna>
2013-05-21 19:04:49 +0000

    * Makefile.in (DLL_OFILES): Add arc4random.o. 	* common.din: Export arc4random, arc4random_addrandom, arc4random_buf, 	arc4random_stir and arc4random_uniform. 	* mktemp.cc (arc4random): Remove static replacement function. 	* posix.sgml (std-bsd): Add arc4random functions. 	* include/cygwin/stdlib.h: Declare arc4random functions. 	* include/cygwin/version.h (CYGWIN_VERSION_API_MINOR): Bump. 	* libc/arc4random.cc: New file implementing arc4random functions taken 	from FreeBSD.

--- winsup/cygwin/include/cygwin/version.h
+++ winsup/cygwin/include/cygwin/version.h
@@ -433,12 +433,15 @@ details. */
       262: Export getmntent_r.
       263: Export cfsetspeed.
       264: Consistently export strtold
+      265: Export __b64_ntop, __b64_pton.
+      266: Export arc4random, arc4random_addrandom, arc4random_buf,
+	   arc4random_stir, arc4random_uniform.
      */
 
      /* Note that we forgot to bump the api for ualarm, strtoll, strtoull */
 
 #define CYGWIN_VERSION_API_MAJOR 0
-#define CYGWIN_VERSION_API_MINOR 264
+#define CYGWIN_VERSION_API_MINOR 266
 
      /* There is also a compatibity version number associated with the
 	shared memory regions.  It is incremented when incompatible

9c219e7be7da98b737b619886cd1c6142d6fa8b4
kseitz <kseitz>
2013-06-05 20:19:46 +0000

    * generic/gdbtk.c (tk_command): Use xfree instead of free. 	* generic/gdbtk-hooks.c (gdbtk_call_command): Remove unused 	cleanup. 	* generic/gdbtk-cmds.c (gdb_load_info): If bfd_check_format 	fails, run the cleanup.

--- gdb/gdbtk/generic/gdbtk-hooks.c
+++ gdb/gdbtk/generic/gdbtk-hooks.c
@@ -1,7 +1,7 @@
 /* Startup code for Insight.
 
    Copyright (C) 1994, 1995, 1996, 1997, 1998, 2000, 200, 2002, 2003, 2004,
-   2008, 2010, 2011, 2012 Free Software Foundation, Inc.
+   2008, 2010, 2011, 2012, 2013 Free Software Foundation, Inc.
 
    Written by Stu Grossman <grossman@cygnus.com> of Cygnus Support.
 
@@ -528,9 +528,6 @@ static void
 gdbtk_call_command (struct cmd_list_element *cmdblk,
 		    char *arg, int from_tty)
 {
-  struct cleanup *old_chain;
-
-  old_chain = make_cleanup (null_cleanup, 0);
   running_now = 0;
   if (cmdblk->class == class_run || cmdblk->class == class_trace)
     {
@@ -545,8 +542,6 @@ gdbtk_call_command (struct cmd_list_element *cmdblk,
     }
   else
     cmd_func (cmdblk, arg, from_tty);
-
-  do_cleanups (old_chain);
 }
 
 /* Called after a `set' command succeeds.  Runs the Tcl hook
